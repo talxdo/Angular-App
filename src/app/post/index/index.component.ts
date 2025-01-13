66e3f708852b29480b8a0902abf7a15808cbeda8
talxdo <mccn.tala [at] gmail.com>
2025-01-13 14:23:51 -0500

    add providerHttpClient

--- src/app/post/index/index.component.ts
+++ src/app/post/index/index.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-index',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit{
+  
+  public postservice = inject(PostService);
+  
+  posts : Post[] = [];
+
+  ngOnInit(): void {
+    this.postservice.getAll().subscribe((data : Post[]) => {
+      this.posts = data;
+      console.log(this.posts);
+    })
+  }
+
+  deletePost(id: number){
+    this.postservice.delete(id).subscribe(res => {
+      this.posts = this.posts.filter(
+        item => item.id !== id);
+      console.log('Post eliminado correctamente')
+    })
+
+  }
 
 }
