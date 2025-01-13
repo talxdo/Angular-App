8f7ffd7f196db50647633e45ac0ce34e143ee3f0
talxdo <mccn.tala [at] gmail.com>
2025-01-13 13:58:23 -0500

    rutas ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './post/index/index.component';
+import { CreateComponent } from './post/create/create.component';
+import { ViewComponent } from './post/view/view.component';
+import { EditComponent } from './post/edit/edit.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'post/index',
+        pathMatch: 'full'
+    },
+    {
+        path: 'post/index',
+        component: IndexComponent
+    },
+    {
+        path: 'post/create',
+        component: CreateComponent
+    },
+    {
+        path: 'post/:idPost/view',
+        component: ViewComponent
+    },
+    {
+        path: 'post/:idPost/edit',
+        component: EditComponent
+    }
+];
